0c73d186adf4a1379454831086dd9841dae489d4
Santiago <santirogo@gmail.com>
2019-03-13 14:09:11 -0500

    Vistas 2, 4 y 7 creadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { LoginComponent } from '../login/login.component';
+import { MenuComponent } from '../menu/menu.component';
+import { RegistryComponent } from '../registry/registry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, LoginComponent, MenuComponent, RegistryComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     AppRoutingModule, BrowserAnimationsModule
   ],
