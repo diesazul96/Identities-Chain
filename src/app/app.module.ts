f7ddbfa816f94f632f5a635727adf73256293bed
nicolasOtalora <nicolas.otalora@correo.usa.edu.co>
2019-03-13 12:12:18 -0500

    Vistas de Nuevo Documento y Nueva Cédula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DocumentoComponent} from '../documento/documento.component';
+import {CedulaComponent} from '../nueva-cedula/nuevacedula.component';
+
+
+//Dropdowns Material Imports
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import{ MatInputModule} from '@angular/material/input';
+
+
+//Imports para el datepicker
+import{ MatDatepickerModule} from '@angular/material/datepicker';
+import{MatNativeDateModule} from '@angular/material';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentoComponent,
+    CedulaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, BrowserAnimationsModule
+    AppRoutingModule, BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
