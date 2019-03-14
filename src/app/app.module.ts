20f937a75c9943a1dc73b349ff0e8b8286e9ee60
mauricionietom <30412495+mauricionietom@users.noreply.github.com>
2019-03-14 10:59:55 -0500

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { PasswordComponent } from './password/password.component';
+import { MailpasswordComponent } from './mailpassword/mailpassword.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PasswordComponent,
+    MailpasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mailpassword/mailpassword.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mailpassword',
+  templateUrl: './mailpassword.component.html',
+  styleUrls: ['./mailpassword.component.css']
+})
+export class MailpasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
