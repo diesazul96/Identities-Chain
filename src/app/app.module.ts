7338b4cfe6cd793290102a80574f5e3949db30ef
diesazul96 <diesazul96@hotmail.com>
2019-03-13 22:00:27 -0500

    Dos vistas completas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { VisualizadorDocumento } from './visualizador-documento/visualizador-documento.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatFormFieldModule, MatInputModule }from '@angular/material';
+import { AñadirDocumento } from './añadir-documento';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, VisualizadorDocumento, AñadirDocumento
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, BrowserAnimationsModule
+    AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatFormFieldModule, MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/visualizador-documento/visualizador-documento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'visualizador-documento',
+    templateUrl: 'visualizador-documento.html',
+    styleUrls: ['visualizador-documento.css']
+})
+export class VisualizadorDocumento {
+
+  constructor() {
+  }
+}
