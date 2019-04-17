5de50985e40511a12928394be5c1a0d461eeddce
Santiago <santirogo@gmail.com>
2019-04-17 14:16:10 -0500

    Se añaden usuarios a firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { LoginComponent } from '../login/login.component';
 import { MenuComponent } from '../menu/menu.component';
 import { RegistryComponent } from '../registry/registry.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,9 +19,10 @@ import { RegistryComponent } from '../registry/registry.component';
   imports: [
     NgbModule,
     BrowserModule,
-    AppRoutingModule, BrowserAnimationsModule
+    AppRoutingModule, BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/registry/registry.component.ts
+++ src/registry/registry.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AngularFirestore , AngularFirestoreCollection} from '@angular/fire/firestore';
+import { User } from './user';
+
 
 @Component({
   selector: 'app-registry',
@@ -6,7 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registry.component.css']
 })
 export class RegistryComponent implements OnInit {
-  constructor() {  }
+
+  public users: User[];
+  private db: AngularFirestore;
+  userCollectionRef: AngularFirestoreCollection<User>;
+  public same = true;
+
+
+  constructor(db: AngularFirestore) {
+    this.db = db;
+    this.userCollectionRef = db.collection<User>('users');
+
+    this.userCollectionRef.snapshotChanges().subscribe(data => {
+      if(data){
+        this.users = data.map( item =>{
+          const data = item.payload.doc.data() as User; //trae los datos de firebase
+          data.id = item.payload.doc.id; //trae el id del documento creado een firebase
+          return data;
+        });
+      }
+    },
+    err => console.log('Error ' + err),
+    () => console.log('yay'))
+  }
 
   ngOnInit() {}
+
+  addUser(user: string, pass: string, pass2: string){
+    let us = new User();
+    us.user = user;
+    if(pass == pass2){
+      us.password = pass;
+    }
+
+    return this.db.collection('users').add({
+      user: us.user, password: us.password
+    });
+  }
 }
