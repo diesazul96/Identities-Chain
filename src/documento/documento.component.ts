1eb1a26582d89ddc92962b7a860fee0b7c40118a
nicolasOtalora <nicolas.otalora@correo.usa.edu.co>
2019-04-24 20:24:00 -0500

    Cambio chiquito chiquito

--- src/documento/documento.component.ts
+++ src/documento/documento.component.ts
@@ -14,11 +14,14 @@ export class DocumentoComponent implements OnInit{
   }
 
   fetchRowData(rowVal) {
+    console.log("aeafasfasfasf");
+    console.log(rowVal);
+
     let route = this.router.config.find(r => r.path === 'nueva-cedula/:key');
     route.data =  rowVal;
 
   // START: One way of using routerLink
-   this.router.navigateByUrl(`${'nueva-cedula'}/${rowVal.id}`);
+   this.router.navigateByUrl(`${'nueva-cedula'}/${rowVal}`);
    // Uncomment this line and check the result
   // END: One way of using routerLink */
 
